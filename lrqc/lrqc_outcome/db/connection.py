ee79c31c5bb153c3bbd676f7ce40d6db0b141438
Adam Blanchet <ab59@sanger.ac.uk>
2022-04-29 11:46:36 +0000

    Complete lrqc DB ORM schema with relationships.
    Signed-off-by: Adam Blanchet <ab59@sanger.ac.uk>

---
+++ lrqc/lrqc_outcome/db/connection.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+config = {"LQRC_DB_URL": os.environ.get("DB_URL"), "TEST": os.environ.get("LRQC_MODE")}
+
+if config["TEST"]:
+    # config["LRQC_DB_URL"] = "sqlite+pysqlite:///:memory:"
+    config["LRQC_DB_URL"] = "sqlite+pysqlite:///test.db"
+
+if config["LQRC_DB_URL"] is None or config["LQRC_DB_URL"] == "":
+    raise Exception(
+        "ENV['LRQC_DB_URL'] must be set with a database URL, or LRQC_MODE must be set for testing."
+    )
+
+# engine = create_engine(config["LQRC_DB_URL"])
+engine = create_engine("sqlite+pysqlite:///test.db")
+lrqc_session_factory: sessionmaker = sessionmaker(engine, expire_on_commit=False)
--- lrqc/lrqc_outcome/db/db_schema.py
+++ lrqc/lrqc_outcome/db/db_schema.py
@@ -1,7 +1,5 @@
-from black import json
-
-from sqlalchemy import JSON, Column, Integer, String, DateTime
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import JSON, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -18,7 +16,7 @@ class Entity(Base):
     description_sha = Column(
         String(64),
         unique=True,
-        comment="sha256 of the description string (see 'pacbio_ent) or an ordered list of description strings",
+        comment="sha256 of the description string (see 'pacbio_ent') or an ordered list of description strings",
     )
     description = Column(
         String(),
@@ -28,6 +26,12 @@ class Entity(Base):
         JSON, nullable=True, comment="An optional JSON representation of the entity"
     )
     platform_name = Column(String(64), comment="As pacbio or ont")
+
+    annotations = relationship(
+        "Annotation", secondary="entity_annotation", back_populates="entities"
+    )
+    qc_outcome = relationship("QcOutcome", back_populates="entity", uselist=False)
+    qc_outcome_history = relationship("QcOutcomeHistory", back_populates="entity")
 
 
 class PacbioEnt(Base):
@@ -42,27 +46,43 @@ class PacbioEnt(Base):
         comment="A human-readable unique string listing the four attributes in the order they are presented here.",
     )
 
+
 class EntityPacbioEnt(Base):
     __tablename__ = "entity_pacbio_ent"
 
     id_entity_pacbio_ent = Column(Integer, primary_key=True)
-    # TODO: make foreign key
     id_entity = Column(String(), comment="Foreign key, see 'entity'")
     # TODO: make foreign key
     id_pacbio_ent = Column(String(), comment="Foreign key, see 'pacbio_ent'")
+
 
 class QcOutcome(Base):
     __tablename__ = "qc_outcome"
 
     id_qc_outcome = Column(Integer, primary_key=True)
     # TODO: make foreign key
-    id_entity = Column(Integer, comment="Foreign key, see 'entity'")
+    id_entity = Column(
+        Integer, ForeignKey("entity.id_entity"), comment="Foreign key, see 'entity'"
+    )
     # TODO: make foreign key
-    id_qc_outcome_dict = Column(Integer, comment="Foreign key, see 'qc_outcome_dict'")
+    id_qc_outcome_dict = Column(
+        Integer,
+        ForeignKey("qc_outcome_dict.id_qc_outcome_dict"),
+        comment="Foreign key, see 'qc_outcome_dict'",
+    )
     date_created = Column(DateTime)
     date_updated = Column(DateTime)
-    user_name = Column(String(), comment="Real logged user, the user running a script or an agent (a pipeline)")
+    user_name = Column(
+        String(),
+        comment="Real logged user, the user running a script or an agent (a pipeline)",
+    )
     created_by = Column(String(), comment="Application or script name, RT ticket, etc.")
+
+    entity = relationship("Entity", back_populates="qc_outcome", uselist=False)
+    qc_outcome_dict = relationship(
+        "QcOutcomeDict", back_populates="qc_outcome", uselist=False
+    )
+
 
 class QcOutcomeDict(Base):
     __tablename__ = "qc_outcome_dict"
@@ -71,34 +91,73 @@ class QcOutcomeDict(Base):
     description = Column(String(), unique=True, comment="Short description")
     long_description = Column(String(), comment="Long description")
 
+    qc_outcome = relationship(
+        "QcOutcome", back_populates="qc_outcome_dict", uselist=False
+    )
+    qc_outcome_history = relationship(
+        "QcOutcomeHistory", back_populates="qc_outcome_dict", uselist=False
+    )
+
+
 class QcOutcomeHistory(Base):
     __tablename__ = "qc_outcome_history"
 
     id_qc_outcome_history = Column(Integer, primary_key=True)
     # TODO: make foreign key
-    id_entity = Column(Integer, comment="Foreign key, see 'entity'")
+    id_entity = Column(
+        Integer, ForeignKey("entity.id_entity"), comment="Foreign key, see 'entity'"
+    )
     # TODO: make foreign key
-    id_qc_outcome_dict = Column(Integer, comment="Foreign key, see 'qc_outcome_dict'")
+    id_qc_outcome_dict = Column(
+        Integer,
+        ForeignKey("qc_outcome_dict.id_qc_outcome_dict"),
+        comment="Foreign key, see 'qc_outcome_dict'",
+    )
     date_created = Column(DateTime)
     date_updated = Column(DateTime)
-    user_name = Column(String(), comment="Real logged user, the user running a script or an agent (a pipeline)")
+    user_name = Column(
+        String(),
+        comment="Real logged user, the user running a script or an agent (a pipeline)",
+    )
     created_by = Column(String(), comment="Application or script name, RT ticket, etc.")
+
+    entity = relationship("Entity", back_populates="qc_outcome_history", uselist=False)
+    qc_outcome_dict = relationship(
+        "QcOutcomeDict", back_populates="qc_outcome_history", uselist=False
+    )
+
 
 class Annotation(Base):
     __tablename__ = "annotation"
 
     id_annotation = Column(Integer, primary_key=True)
     annotation = Column(String(), comment="Long string")
-    user_name = Column(String(), comment="Real logged userm the user running a script or an agent (a pipeline)")
+    user_name = Column(
+        String(),
+        comment="Real logged user the user running a script or an agent (a pipeline)",
+    )
     date_created = Column(DateTime, comment="Date and time the annotation was created")
+
+    entities = relationship(
+        "Entity", secondary="entity_annotation", back_populates="annotations"
+    )
+
 
 class EntityAnnotation(Base):
     __tablename__ = "entity_annotation"
 
     id_entity_annotation = Column(Integer, primary_key=True)
-    # TODO: make foreign key
-    id_entity = Column(Integer, comment="Foreign key, see 'entity'")
-    # TODO: make foreign key
-    id_annotation = Column(Integer, comment="Foreign key, see 'annotation'")
-    # TODO: make foreign key
-    id_qc_outcome = Column(Integer, nullable=True, comment="If defined, a foreign key, see 'qc_outcome'")
+    id_entity = Column(
+        Integer, ForeignKey("entity.id_entity"), comment="Foreign key, see 'entity'"
+    )
+    id_annotation = Column(
+        Integer,
+        ForeignKey("annotation.id_annotation"),
+        comment="Foreign key, see 'annotation'",
+    )
+    id_qc_outcome = Column(
+        Integer,
+        ForeignKey("qc_outcome.id_qc_outcome"),
+        nullable=True,
+        comment="If defined, a foreign key, see 'qc_outcome'",
+    )
